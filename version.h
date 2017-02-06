a2eca063e06f81d7e6f453449389167ff3e6b530
Serhii Vyshnevetskyi <sergwish@mail.ru>
2017-02-06 10:47:15 +0200

    Increment version.
    Signed-off-by: Serhii Vyshnevetskyi <sergwish@mail.ru>

--- version.h
+++ version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 0;
-	static const long BUILD  = 3;
-	static const long REVISION  = 15;
+	static const long BUILD  = 4;
+	static const long REVISION  = 16;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 6;
-	#define RC_FILEVERSION 0,0,3,15
-	#define RC_FILEVERSION_STRING "0, 0, 3, 15\0"
-	static const char FULLVERSION_STRING [] = "0.0.3.15";
+	static const long BUILDS_COUNT  = 13;
+	#define RC_FILEVERSION 0,0,4,16
+	#define RC_FILEVERSION_STRING "0, 0, 4, 16\0"
+	static const char FULLVERSION_STRING [] = "0.0.4.16";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 3;
+	static const long BUILD_HISTORY  = 4;
 	
 
 #endif //VERSION_H
